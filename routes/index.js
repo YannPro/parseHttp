715a137306e1d60369f036631ec95b933839ab03
YannPro <849723885@qq.com>
2017-11-26 14:50:49 +0800

    2017-11-25 14:50

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
-
+var fs = require('fs');
+require('../util/CommonUtil');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    console.log("headers-ip = " + JSON.stringify(req.headers.host));// 包含了各种header，包括x-forwarded-for(如果被代理过的话)
+    var time = new Date().Format('yyyy-MM-dd hh:mm:ss');
+    console.log(time)
+    var ip = req.connection.remoteAddress;
+    if (ip.substr(0, 7) == "::ffff:") {
+        ip = ip.substr(7);
+    }
+    console.log('ip:'+ ip);
+
+    fs.exists("../ip.txt", function(exists) {
+        if(!exists){
+            fs.open('../ip.txt','',function () {
+                
+            });
+        }
+        fs.appendFile('../ip.txt','ip地址:'+ip+','+'时间:'+time+'\n',(err,data) => {
+            console.log("写入成功");
+        });
+    });
     res.render('index', { title: 'Express' });
 });
 
